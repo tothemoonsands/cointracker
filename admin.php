c185eb72be03379c5c1a37754147d6308b139217
tothemoonsands <tothemoonsands@users.noreply.github.com>
2017-07-17 17:01:46 -0500

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,100 @@
+<?php
+
+require (__DIR__.'db.php');
+
+$sql = "SELECT hold FROM onhold WHERE id=(SELECT MAX(id) FROM onhold)";
+
+ if ($result = mysqli_query($db, $sql)) {
+
+           while ($row = mysqli_fetch_assoc($result)) {
+           	$hold = $row['hold'];
+
+           }
+        }
+
+?>
+
+<HTML>
+
+<head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+
+
+
+
+</head>
+
+<body>
+
+<h2>FoS Admin Area</h2>
+</br>
+
+<form class="form-horizontal" id="payoutform" action="payout.php" method="post">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Add payout</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Amount">Amount</label>  
+  <div class="col-md-4">
+  <input id="Amount" name="Amount" type="text" placeholder="payout amount" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Submit">Submit</label>
+  <div class="col-md-4">
+
+    <button id="Submit" type="Submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+
+<form id="holdform" class="form-horizontal" action="edit.php" method="post">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Edit On Hold Amount</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="hold">On-Hold</label>  
+  <div class="col-md-4">
+
+  <input id="hold" name="hold" type="text" value="<?php echo $hold?>" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Submit"></label>
+  <div class="col-md-4">
+
+    <button id="Submit2" type="submit" class="btn btn-primary">Edit</button>
+  </div>
+</div>
+</fieldset>
+</form>
+
+
+
+    
+
+</body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+
+
+  <link rel="stylesheet" href="custom.css" >
+
+</head>
+
+
+
+<body>
+  <section>
+    <div class="container">
+     <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <hgroup>
+          <h2>
+            Coin Lookup
+          </h2>
+        </hgroup>
+        <div class="well">
+         <form action="search.php" method="request">
+           <div class="input-group input-group-lg">
+             
+             <input id="search" class="form-control" name="search" type="text" placeholder="Firstbits">
+
+             <span class="input-group-btn">
+               <input type = 'submit' value = 'Submit' class="btn btn-info btn-lg">Submit</button>
+             </span>
+           </div>
+
+         </form>
+       </div>
+     </div>
+   </div>
+ </div>
+</section>
+
+
+</body>
+</html>
